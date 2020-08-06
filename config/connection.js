9bf03487f9f2d6792de4675421cce67ee0c1582f
Andrew Souza <drewmargielaa@gmail.com>
2020-08-06 18:42:30 -0400

    sql

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ var mysql = require("mysql");
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL)
 }else{
-var connection = mysql.createConnection({
+  connection = mysql.createConnection({
   host: "localhost",
   port: 3000,
   user: "root",
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,3 @@
-  
 INSERT INTO burgers (burger_name) VALUES ('Burger1');
 INSERT INTO burgers (burger_name) VALUES ('Burger2');
 INSERT INTO burgers (burger_name) VALUES ('Burger3');
